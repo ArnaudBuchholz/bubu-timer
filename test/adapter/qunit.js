a8bed8dd20b9c89c6f5a578c93bc99a028f65782
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-07-18 13:11:25 -0400

    bdd -> qUnit adapter

---
+++ test/adapter/qunit.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let _labels = [];
+
+global.describe = (label, content) => {
+    _labels.push(label);
+    QUnit.module(_labels.join(" / "));
+    content();
+    _labels.pop();
+};
+
+global.it = (label, callback) => {
+    if (callback) {
+        QUnit.test(label, assert => {
+            if (callback.length) {
+                var done = assert.async();
+                callback(() => {
+                    assert.ok(true);
+                    done();
+                });
+            } else {
+                callback();
+                assert.ok(true);
+            }
+        });
+    } else {
+        QUnit.skip(label);
+    }
+};
+
+global.assert = require("assert");
