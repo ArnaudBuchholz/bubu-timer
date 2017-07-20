a9bde2f7c785e6227a4225759651ed34775bad7d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-07-19 23:45:14 -0400

    WIP

--- test/tick-generator.js
+++ test/tick-generator.js
@@ -1,11 +1,40 @@
 "use strict";
 
-// import tickGenerator from "../src/tick-generator";
+const tickGenerator = require("../src/tick-generator");
 
 describe("tick-generator", function () {
 
-    it("exists", function () {
-        assert(true);
+    it("exists", () => {
+        assert(tickGenerator);
+    });
+
+    it("allocates a tick generator", () => {
+        let ticker = tickGenerator.allocate([10]);
+        assert(ticker);
+    });
+
+    it("exposes tick, pause and resume methods", () => {
+        let ticker = tickGenerator.allocate([10]);
+        ["tick", "pause", "resume"].forEach(methodName => {
+            assert("function" === typeof ticker[methodName]);
+        });
+    });
+
+    it("exposes on callback handler", () => {
+        let ticker = tickGenerator.allocate([10]);
+        assert("function" === typeof ticker.on);
+    });
+
+    it("triggers a tick upon callback attachment", (done) => {
+        let ticker = tickGenerator.allocate([10]);
+        ticker.on((tick) => {
+            try {
+                assert(tick.paused === true);
+                done();
+            } catch (e) {
+                done(e);
+            }
+        })
     });
 
 });
