8e630c63e0a5faee944a22e4641946b301ab607c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-07-21 13:40:20 -0400

    New test

---
+++ test/tick-converter.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const tickConverter = require("../src/tick-converter");
+
+describe("tick-converter", function () {
+
+    it("is a function", () => {
+        assert("function" === typeof tickConverter);
+    });
+
+});
