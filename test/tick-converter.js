fc2733b21154393ea40b4ff119fd6ea84e66102f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-07-22 07:23:08 -0400

    Testing tick converter

--- test/tick-converter.js
+++ test/tick-converter.js
@@ -8,4 +8,35 @@ describe("tick-converter", function () {
         assert("function" === typeof tickConverter);
     });
 
+    [{
+        label: "No elapsed time",
+        tick: 0,
+        sequence: [1000],
+        expectedStep: 0,
+        expectedRemaining: 1000
+    }, {
+        label: "Elapsed in first step",
+        tick: 123,
+        sequence: [1000],
+        expectedStep: 0,
+        expectedRemaining: 877
+    }, {
+        label: "First step fully elapsed",
+        tick: 1000,
+        sequence: [1000],
+        expectedStep: 1,
+        expectedRemaining: 0
+
+    }].forEach(test => {
+
+        let {label, tick, sequence, expectedStep, expectedRemaining} = test;
+
+        it(`gives remaining time from sequence: ${label}`, () => {
+            let result = tickConverter(tick, sequence);
+            assert(result.step === expectedStep);
+            assert(result.remaining = expectedRemaining);
+        });
+
+    });
+
 });
