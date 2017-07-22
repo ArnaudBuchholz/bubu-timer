34ab388604523007aa446fd670d94a5fda233cd2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-07-22 07:39:23 -0400

    Tests are passing

--- src/tick-converter.js
+++ src/tick-converter.js
@@ -1,5 +1,20 @@
 "use strict";
 
 module.exports = (elapsed, sequence) => {
-
+    let step = 0,
+        remaining = 0;
+    sequence.forEach((ms) => {
+        if (elapsed >= ms) {
+            elapsed -= ms;
+            ++step;
+            return true;
+        } else {
+            remaining = ms - elapsed;
+            if (remaining === 0) {
+                ++step;
+            }
+            return false;
+        }
+    });
+    return {step, remaining};
 };
