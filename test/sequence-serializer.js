e31fe9ba4ce0fd1ca101be5b130f547e6f1c55da
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-07-27 12:47:22 -0400

    Sequence serializer tests

---
+++ test/sequence-serializer.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const
+    sequenceSerializer = require("../src/sequence-serializer"),
+
+    tests = [{
+        string: "1",
+        sequence: [1000]
+    }, {
+        string: "1,2,3",
+        sequence: [1000,2000,3000]
+    }];
+
+describe("sequence-serializer", () => {
+
+    it("exposes read and write", () => {
+        assert("function" === typeof sequenceSerializer.read);
+        assert("function" === typeof sequenceSerializer.write);
+    });
+
+    describe("write", () => {
+
+        tests.forEach(({string, sequence}) => {
+            it(`${JSON.stringify(sequence)} => ${string}`, function () {
+                assert(string === sequenceSerializer.write(sequence));
+            });
+        });
+
+    });
+
+    describe("read", () => {
+
+        tests.forEach(({string, sequence}) => {
+            it(`${string} => ${JSON.stringify(sequence)}`, function () {
+                let result = sequenceSerializer.read(string);
+                assert(result.length === sequence.length);
+                sequence.forEach((count, index) => assert(count === result[index]));
+            });
+        });
+
+    });
+
+});
+
