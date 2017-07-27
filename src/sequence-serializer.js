12dfc0af3d00530b650b7135b1308b7ccb95d92e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-07-27 12:47:08 -0400

    Sequence serializer

---
+++ src/sequence-serializer.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const
+    _read = (string) => string
+        .split(",")
+        .map(time => 1000 * parseInt(time, 10)),
+
+    _write = (sequence) => sequence
+        .map(time => Math.floor(time / 1000))
+        .join(",")
+;
+
+module.exports = {
+    read: _read,
+    write: _write
+};
