f9ea372ef681bd1741f9abd3c0423f0826aa88ad
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-07-31 12:47:02 -0400

    ESLint fixes

--- src/run.js
+++ src/run.js
@@ -1,5 +1,8 @@
+"use strict";
+
 const
     gpf = require("gpf-js"),
     sequenceSerializer = require("./sequence-serializer");
 
+/*eslint-disable no-alert, no-undef*/
 alert(`gpf: ${gpf.version()}, sequenceSerializer: ${sequenceSerializer ? "OK" : "KO"}`);
