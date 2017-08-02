db04b55215331b51d50d2128361c2eb08319cc57
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-02 15:19:16 -0400

    ESLint fixes

--- src/tick-formatter.js
+++ src/tick-formatter.js
@@ -3,7 +3,7 @@
 module.exports = (tick) => {
     let
         ms = tick % 1000,
-        seconds = (tick - ms)/ 1000,
+        seconds = (tick - ms) / 1000,
         s = seconds % 60,
         m = (seconds - s) / 60;
     return {ms, s, m};
