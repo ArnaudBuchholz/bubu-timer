231844dc2ac021ca50b94440d20dfcd2a2f3cbbd
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-03 14:48:17 -0400

    WIP

--- src/run.js
+++ src/run.js
@@ -18,37 +18,51 @@ const
         || defaultRequestAnimFrame,
 
     sequence = sequenceSerializer.read(location.search.substr(1)),
+    sequenceTotal = sequence.reduce((total, tick) => total + tick, 0),
 
     ticker = tickGenerator.allocate(),
 
-    onTick = tick => {
+    ratio2Coords = (ratio, radius = 1, precision = 10000) => {
+        let radian = ratio * 2 * Math.PI,
+            x = Math.floor(radius * precision * Math.sin(radian)) / precision,
+            y = Math.floor(-radius * precision * Math.cos(radian)) / precision;
+        return {x, y};
+    },
 
-        let deg = 360 * tick.elapsed / 200 % 360,
-            rad = deg * 2 * Math.PI / 360,
-            toX1 = Math.floor(9900 * Math.sin(rad)) / 10000,
-            toY1 = Math.floor(-9900 * Math.cos(rad)) / 10000,
-            toX2 = Math.floor(8900 * Math.sin(rad)) / 10000,
-            toY2 = Math.floor(-8900 * Math.cos(rad)) / 10000,
-            secondHalf = deg > 180,
+    getCirclePath = (ratio, outerRadius, innerRadius) => {
+        let
+            outer = ratio2Coords(ratio, outerRadius),
+            inner = ratio2Coords(ratio, innerRadius),
             path = [
-                "M 0 -.99"
+                `M 0 -${outerRadius}`
             ];
-        if (deg > 180) {
-            path.push("A .99 .99 0 0 1 0 .99",
-                `A .99 .99 0 0 1 ${toX1} ${toY1}`,
-                `L ${toX2} ${toY2}`,
-                "A .89 .89 0 0 0 0 .89",
-                "A .89 .89 0 0 0 0 -.89"
+        if (ratio > 0.5) {
+            path.push(`A ${outerRadius} ${outerRadius} 0 0 1 0 ${outerRadius}`,
+                `A ${outerRadius} ${outerRadius} 0 0 1 ${outer.x} ${outer.y}`,
+                `L ${inner.x} ${inner.y}`,
+                `A ${innerRadius} ${innerRadius} 0 0 0 0 ${innerRadius}`,
+                `A ${innerRadius} ${innerRadius} 0 0 0 0 -${innerRadius}`
             );
         } else {
-            path.push(`A .99 .99 0 0 1 ${toX1} ${toY1}`,
-                `L ${toX2} ${toY2}`,
-                "A .89 .89 0 0 0 0 -.89"
+            path.push(`A ${outerRadius} ${outerRadius} 0 0 1 ${outer.x} ${outer.y}`,
+                `L ${inner.x} ${inner.y}`,
+                `A ${innerRadius} ${innerRadius} 0 0 0 0 -${innerRadius}`
             );
         }
-        path.push("L 0 -.99");
-        document.getElementById("p").setAttribute("d", path.join(" "));
-        ++tick;
+        path.push(`L 0 -${outerRadius}`);
+        return path.join(" ");
+    },
+
+    onTick = tick => {
+
+        let
+            convertedTick = tickConverter(tick.elapsed, sequence),
+            currentDuration = sequence[convertedTick.step % sequence.length],
+            outer = tick.elapsed / sequenceTotal % 1,
+            inner = 1 - convertedTick.remaining / currentDuration;
+
+        document.getElementById("outer").setAttribute("d", getCirclePath(outer, 0.99, 0.89));
+        document.getElementById("inner").setAttribute("d", getCirclePath(inner, 0.89, 0.79));
 
         requestAnimFrame(ticker.tick.bind(ticker));
     },
@@ -79,14 +93,21 @@ const
             circle({cx: 0, cy: 0, r: 0.99, stroke: "white", "stroke-width": 0.01, fill: "blue"}),
             circle({cx: 0, cy: 0, r: 0.89, stroke: "white", "stroke-width": 0.01, fill: "green"}),
             circle({cx: 0, cy: 0, r: 0.79, stroke: "white", "stroke-width": 0.01, fill: "white"}),
-            text({"font-family": "Arial", "font-size": 0.3, x: 0, y: 0.1, "text-anchor": "middle",
+            text({id: "time",
+                "font-family": "Arial", "font-size": 0.3, x: 0, y: 0.1, "text-anchor": "middle",
                 fill: "red", stroke: "black", "stroke-width": 0.01}, "00:00"),
-            text({"font-family": "Arial", "font-size": 0.1, x: 0.65, y: 0.1, "text-anchor": "end",
+            text({id: "ms",
+                "font-family": "Arial", "font-size": 0.1, x: 0.65, y: 0.1, "text-anchor": "end",
                 fill: "red", stroke: "black", "stroke-width": 0.001}, ".123"),
-            text({"font-family": "Arial", "font-size": 0.1, x: 0, y: 0.3, "text-anchor": "middle",
+            text({id: "step",
+                "font-family": "Arial", "font-size": 0.1, x: 0, y: 0.3, "text-anchor": "middle",
                 fill: "red", stroke: "black", "stroke-width": 0.01}, "1 / 2"),
-            path({id: "p", d: "M 0 -.99 A .99 .99 0 0 1 .99 0 L .89 0 A .89 .89 0 0 0 0 -.89 L 0 -.99",
-                fill: "purple", stroke: "black", "stroke-width": 0.01})
+            path({id: "outer",
+                d: "M 0 -.99 A .99 .99 0 0 1 .99 0 L .89 0 A .89 .89 0 0 0 0 -.89 L 0 -.99",
+                fill: "purple", stroke: "black", "stroke-width": 0.01}),
+            path({id: "inner",
+                d: "M 0 -.89 A .89 .89 0 0 1 .89 0 L .79 0 A .79 .79 0 0 0 0 -.79 L 0 -.89",
+                fill: "red", stroke: "black", "stroke-width": 0.01})
 
         ]));
     }
@@ -97,5 +118,7 @@ window.addEventListener("load", () => {
         alert("No sequence to play");
     } else {
         setup();
+        ticker.on(onTick);
+        ticker.resume();
     }
 });
