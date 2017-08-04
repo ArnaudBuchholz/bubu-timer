aaf877c9b8999e55c84912601606d85d4995194e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-08-03 23:14:37 -0400

    Matches UI expectation

--- src/tick-formatter.js
+++ src/tick-formatter.js
@@ -1,10 +1,23 @@
 "use strict";
 
+const _zero = (x, count = 2) => {
+    let result = [],
+        max = Math.pow(10, count - 1);
+    while (max > 1 && x < max) {
+        result.push("0");
+        max /= 10;
+    }
+    result.push(x);
+    return result.join("");
+};
+
+
 module.exports = (tick) => {
     let
-        ms = tick % 1000,
+        ms = _zero(tick % 1000, 3),
         seconds = (tick - ms) / 1000,
-        s = seconds % 60,
-        m = (seconds - s) / 60;
-    return {ms, s, m};
+        s = _zero(seconds % 60),
+        m = _zero((seconds - s) / 60),
+        time = `${m}:${s}`;
+    return {time, ms};
 };
