084a8d3c74e5af1338acccbeff1ed15c20a4bb05
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-04 13:38:13 -0400

    Colors definition

---
+++ src/colors.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+    background: "#ccd0d3",
+    circle: {
+        light: "#dce0e1",
+        shaded: "#c9ced1",
+        dark: "#b3b7ba",
+        background: "#a8acae"
+    },
+    progress: {
+        total: "#def1f2",
+        step: "#f3dfe6"
+    },
+    text: {
+        time: "#ffffff",
+        ms: "#ffffff",
+        step: "#000000"
+    }
+};
