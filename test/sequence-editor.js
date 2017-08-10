d7c6530730316ea92eb5af684cd69b6a4ed4de1d
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-10 13:01:21 -0400

    Sequence editor

--- test/sequence-editor.js
+++ test/sequence-editor.js
@@ -5,7 +5,7 @@ const
 
 describe("sequence-editor", () => {
 
-    ["on", "inc", "dec", "add"].forEach(methodName => {
+    ["on", "inc", "dec", "add", "remove", "get"].forEach(methodName => {
         it(`exposes ${methodName} method`, () => {
             let editor = sequenceEditor.allocate();
             assert("function" === typeof editor [methodName]);
@@ -21,31 +21,44 @@ describe("sequence-editor", () => {
     });
 
     const
-        playScenario = (label, scenario) => {
+        actions = {
+            inc: (editor, seconds) => editor.inc(seconds),
+            dec: (editor, seconds) => editor.dec(seconds),
+            add: editor => editor.add(),
+            remove: editor => editor.remove()
+        },
+        playScenario = (label, scenario, expectedSequence) => {
             it(label, () => {
                 let expected = ["00:00"],
+                    expectedLengthChanged = false,
                     editor = sequenceEditor.allocate(),
                     callbackCount = 0;
-                editor.on(sequence => {
+                editor.on((sequence, lengthChanged) => {
                     assert(sequence.length === expected.length);
                     expected.forEach((ms, pos) => {
                         assert(sequence[pos] === ms);
                     });
+                    assert(expectedLengthChanged === lengthChanged);
                     ++callbackCount;
                 });
                 callbackCount = 0;
                 scenario.forEach((item, index) => {
                     assert(callbackCount === index);
                     expected = item.expected;
-                    if (item.inc) {
-                        editor.inc(item.inc);
-                    } else if (item.dec) {
-                        editor.dec(item.dec);
-                    } else if (item.add) {
-                        editor.add();
-                    }
+                    let action = Object.keys(item).filter(name => name !== "expected")[0];
+                    expectedLengthChanged = undefined !== item.expectedLengthChanged
+                        ? item.expectedLengthChanged
+                        : -1 !== ["add", "remove"].indexOf(action);
+                    actions[action](editor, item[action], item);
                     assert(callbackCount === index + 1);
                 });
+                if (expectedSequence) {
+                    let sequence = editor.get();
+                    assert(expectedSequence.length === sequence.length);
+                    expectedSequence.forEach((tick, index) => {
+                        assert(tick === sequence[index]);
+                    });
+                }
             });
         };
 
@@ -69,7 +82,7 @@ describe("sequence-editor", () => {
         }, {
             inc: 3600,
             expected: ["61:13"]
-        }]);
+        }], [3673000]);
 
         playScenario("scenario 2", [{
             inc: 10,
@@ -80,7 +93,7 @@ describe("sequence-editor", () => {
         }, {
             dec: 1,
             expected: ["00:08"]
-        }]);
+        }], [8000]);
 
         playScenario("scenario 3 (never below 0)", [{
             inc: 10,
@@ -97,7 +110,7 @@ describe("sequence-editor", () => {
         }, {
             dec: 3600,
             expected: ["00:00"]
-        }]);
+        }], [0]);
 
     });
 
@@ -115,9 +128,44 @@ describe("sequence-editor", () => {
         }, {
             dec: 10,
             expected: ["01:00", "00:20"]
-        }]);
+        }], [60000, 20000]);
 
     });
 
+    describe("removing duration", () => {
+
+        playScenario("scenario 1", [{
+            inc: 60,
+            expected: ["01:00"]
+        }, {
+            add: true,
+            expected: ["01:00", "00:00"]
+        }, {
+            inc: 30,
+            expected: ["01:00", "00:30"]
+        }, {
+            remove: true,
+            expected: ["01:00"]
+        }], [60000]);
+
+        playScenario("scenario 2 (always one item)", [{
+            inc: 60,
+            expected: ["01:00"]
+        }, {
+            add: true,
+            expected: ["01:00", "00:00"]
+        }, {
+            inc: 30,
+            expected: ["01:00", "00:30"]
+        }, {
+            remove: true,
+            expected: ["01:00"]
+        }, {
+            remove: true,
+            expected: ["00:00"],
+            expectedLengthChanged: false
+        }], [0]);
+
+    });
 });
 
