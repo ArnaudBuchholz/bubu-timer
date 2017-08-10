92f2a71134fa8e4fd8f39975c7ccfb333bc43fd8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-08-09 23:11:37 -0400

    Editor

--- test/sequence-editor.js
+++ test/sequence-editor.js
@@ -5,74 +5,82 @@ const
 
 describe("sequence-editor", () => {
 
-    it("exposes a known interface", () => {
-        ["on", "reset", "add", "sub"].forEach(name => {
-            assert("function" === typeof sequenceEditor[name]);
+    ["on", "inc", "dec"].forEach(methodName => {
+        it(`exposes ${methodName} method`, () => {
+            let editor = sequenceEditor.allocate();
+            assert("function" === typeof editor [methodName]);
         });
     });
 
-    it("notifies of empty sequence", () => {
-        sequenceEditor.on(sequence => {
+    it("notifies of UI refreshes", () => {
+        let editor = sequenceEditor.allocate();
+        editor.on(sequence => {
             assert(sequence.length === 1);
-            assert(sequence[0] === 0);
+            assert(sequence[0] === "00:00");
         });
-        sequenceEditor.reset();
-    });
-
-    it("resets with a default value", () => {
-        sequenceEditor.on(sequence => {
-            assert(sequence.length === 1);
-            assert(sequence[0] === 1000);
-        });
-        sequenceEditor.reset(1000);
     });
 
     describe("duration modification", () => {
 
-        it("allows adding ms", () => {
-            let expected = [1000];
-            sequenceEditor.on(sequence => {
-                assert(sequence.length === expected.length);
-                expected.forEach((ms, pos) => {
-                    assert(sequence[pos] === ms);
+        const
+            playScenario = (label, scenario) => {
+                it(label, () => {
+                    let expected = ["00:00"],
+                        editor = sequenceEditor.allocate(),
+                        callbackCount = 0;
+                    editor.on(sequence => {
+                        assert(sequence.length === expected.length);
+                        expected.forEach((ms, pos) => {
+                            assert(sequence[pos] === ms);
+                        });
+                        ++callbackCount;
+                    });
+                    callbackCount = 0;
+                    scenario.forEach((item, index) => {
+                        assert(callbackCount === index);
+                        expected = item.expected;
+                        if (item.inc) {
+                            editor.inc(item.inc);
+                        } else if (item.dec) {
+                            editor.dec(item.dec);
+                        }
+                        assert(callbackCount === index + 1);
+                    });
                 });
-            });
-            sequenceEditor.reset(1000);
-            expected = [2000];
-            sequenceEditor.add(1000);
-            expected = [3000];
-            sequenceEditor.add(1000);
-        });
+            };
 
-        it("allows substracting ms", () => {
-            let expected = [1000];
-            sequenceEditor.on(sequence => {
-                assert(sequence.length === expected.length);
-                expected.forEach((ms, pos) => {
-                    assert(sequence[pos] === ms);
-                });
-            });
-            sequenceEditor.reset(1000);
-            expected = [500];
-            sequenceEditor.sub(500);
-            expected = [0];
-            sequenceEditor.sub(500);
-        });
+        playScenario("scenario 1", [{
+            inc: 1,
+            expected: ["00:01"]
+        }, {
+            inc: 1,
+            expected: ["00:02"]
+        }, {
+            inc: 1,
+            expected: ["00:03"]
+        }]);
 
-        it("never goes below 0", () => {
-            let expected = [1000];
-            sequenceEditor.on(sequence => {
-                assert(sequence.length === expected.length);
-                expected.forEach((ms, pos) => {
-                    assert(sequence[pos] === ms);
-                });
-            });
-            sequenceEditor.reset(1000);
-            expected = [0];
-            sequenceEditor.sub(1500);
-            expected = [0];
-            sequenceEditor.sub(500);
-        });
+        playScenario("scenario 2", [{
+            inc: 10,
+            expected: ["00:10"]
+        }, {
+            dec: 1,
+            expected: ["00:09"]
+        }, {
+            dec: 1,
+            expected: ["00:08"]
+        }]);
+
+        playScenario("scenario 3 (never below 0)", [{
+            inc: 10,
+            expected: ["00:10"]
+        }, {
+            dec: 15,
+            expected: ["00:00"]
+        }, {
+            inc: 1,
+            expected: ["00:01"]
+        }]);
 
     });
 
