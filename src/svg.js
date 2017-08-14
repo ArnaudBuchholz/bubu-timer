750b6ed4ec2c8a10196ebe0c9026e1cab26dc678
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-14 12:35:56 -0400

    Common SVG definitions

---
+++ src/svg.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const
+    _tag = function (tagName, properties, children) {
+        let element = document.createElementNS("http://www.w3.org/2000/svg", tagName);
+        Object.keys(properties).forEach(name => {
+            element.setAttribute(name, properties[name]);
+        });
+        [].concat(children || [])
+            .map(def => typeof def === "string" ? document.createTextNode(def) : def)
+            .forEach(node => element.appendChild(node));
+        return element;
+    },
+
+    _svg = _tag.bind(null, "svg");
+
+[
+    "circle",
+    "text",
+    "path",
+    "defs",
+    "linearGradient",
+    "stop"
+
+].forEach(tag => {
+    _svg[tag] = _tag.bind(null, tag);
+});
+
+module.exports = _svg;
