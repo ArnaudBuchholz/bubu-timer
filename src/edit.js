8c14769e4c14100b29b4e26f060def20e7bf7fdd
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-15 13:25:24 -0400

    Implements logic (WIP)

--- src/edit.js
+++ src/edit.js
@@ -6,20 +6,39 @@ const
     svg = require("./svg"),
     colors = require("./colors"),
     gradients = require("./gradients"),
+    sequenceEditor = require("./sequence-editor").allocate(),
 
-    digit = (x, baseId) => [
-        svg.rect({x: x - 0.1, y: -0.8, width: 0.2, height: 0.4,
-            fill: colors.circle.background, stroke: "url(#outerBorder)", "stroke-width": 0.01}),
-        svg.text({id: `inc${baseId}`, x: x, y: -0.8, "font-family": "Arial", "font-size": 0.2, "text-anchor": "middle",
-            fill: colors.text.step, stroke: "url(#innerBorder)", "stroke-opacity": 0.2, "stroke-width": 0.001},
-        "⏶"),
-        svg.text({id: `dig${baseId}`, x: x, y: -0.5, "font-family": "Arial", "font-size": 0.3, "text-anchor": "middle",
-            fill: colors.text.step, stroke: "url(#innerBorder)", "stroke-opacity": 0.2, "stroke-width": 0.001},
-        "0"),
-        svg.text({id: `dec${baseId}`, x: x, y: -0.28, "font-family": "Arial", "font-size": 0.2, "text-anchor": "middle",
-            fill: colors.text.step, stroke: "url(#innerBorder)", "stroke-opacity": 0.2, "stroke-width": 0.001},
-        "⏷")
+    createDigit = (x, baseId) => {
+        const textProperties = {
+            "font-family": "Arial", "font-size": 0.25, "text-anchor": "middle",
+            fill: colors.text.step, stroke: "url(#innerBorder)", "stroke-opacity": 0.2, "stroke-width": 0.001
+        };
+        return [
+            svg.rect({x: x - 0.1, y: -0.8, width: 0.2, height: 0.4,
+                fill: colors.circle.background, stroke: "url(#outerBorder)", "stroke-width": 0.01}),
+            svg.text(Object.assign({id: `inc${baseId}`, x: x, y: -0.8}, textProperties), "⏶"),
+            svg.text(Object.assign({id: `dig${baseId}`, x: x, y: -0.52}, textProperties), ""),
+            svg.text(Object.assign({id: `dec${baseId}`, x: x, y: -0.26}, textProperties), "⏷")
+        ];
+    },
+
+    createButton = ({id, cx, x, y, label}) => [
+        svg.g({id: id}, [
+            svg.circle({r: 0.15, cx: cx, cy: 0.7,
+                fill: colors.circle.light, stroke: "url(#innerBorder)", "stroke-width": 0.01}),
+            svg.text({x: x, y: y, "font-family": "Arial", "font-size": 0.2, "text-anchor": "middle",
+                fill: colors.text.step, stroke: "url(#outerBorder)", "stroke-opacity": 0.2, "stroke-width": 0.001},
+            label)
+        ])
     ],
+
+    refresh = (sequence/*, lengthChanged*/) => {
+        let current = sequence[sequence.length - 1];
+        [0, 1, 3, 4].forEach((pos, digit) => {
+            document.getElementById(`dig${digit}`).innerHTML = current.substr(pos, 1);
+        });
+        // alert(sequence, lengthChanged);
+    },
 
     setup = () => {
         document.body.appendChild(svg({
@@ -28,10 +47,42 @@ const
             viewBox: "-1 -1 2 2",
             style: `background-color: ${colors.background};`
         }, [gradients()]
-            .concat(digit(-0.4, 0), digit(-0.15, 1), digit(0.15, 2), digit(0.4, 3))
+            .concat(
+                createDigit(-0.4, 0),
+                createDigit(-0.15, 1),
+                createDigit(0.15, 2),
+                createDigit(0.4, 3)
+            )
+            .concat(
+                createButton({id: "remove", cx: -0.4, x: -0.4, y: 0.75, label: "⎌"}),
+                createButton({id: "add", cx: 0, x: 0, y: 0.77, label: "+"}),
+                createButton({id: "run", cx: 0.4, x: 0.42, y: 0.77, label: "▶"})
+            )
         ));
-    }
-;
+        sequenceEditor.on(refresh);
+    },
+
+    noop = () => {},
+
+    mapping = {
+        inc0: () => sequenceEditor.inc(600),
+        dec0: () => sequenceEditor.dec(600),
+        inc1: () => sequenceEditor.inc(60),
+        dec1: () => sequenceEditor.dec(60),
+        inc2: () => sequenceEditor.inc(10),
+        dec2: () => sequenceEditor.dec(10),
+        inc3: () => sequenceEditor.inc(1),
+        dec3: () => sequenceEditor.dec(1)
+    };
 
 window.addEventListener("load", setup);
 
+window.addEventListener("click", (e) => {
+    let target = e.target,
+        id;
+    while (!id && target) {
+        id = target.id;
+        target = target.parentNode;
+    }
+    (mapping[id] || noop)();
+});
