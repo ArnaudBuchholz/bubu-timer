6062940c0d2db9135b944becd7e616a359aaf767
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-18 13:08:45 -0400

    Removes sound until properly managed

--- src/run.js
+++ src/run.js
@@ -15,8 +15,8 @@ const
     tickConverter = require("./tick-converter"),
     tickFormatter = require("./tick-formatter"),
 
-    alarm1 = new Audio(require("./res/alarm1.mp3")),
-    alarm2 = new Audio(require("./res/alarm2.mp3")),
+    // alarm1 = new Audio(require("./res/alarm1.mp3")),
+    // alarm2 = new Audio(require("./res/alarm2.mp3")),
 
     defaultRequestAnimFrame = callback => setTimeout(callback, 1000 / 60),
 
@@ -149,10 +149,8 @@ window.addEventListener("load", () => {
 
 window.addEventListener("click", () => {
     if (ticker.isPaused()) {
-        alarm1.play();
         ticker.resume();
     } else {
-        alarm2.play();
         ticker.pause();
     }
 });
