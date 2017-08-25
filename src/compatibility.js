fd7d4be861db8d4fe1c23f53a963efe0420c812e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-08-25 13:00:36 -0400

    Handling Audio and NoSleep incompatibilities

--- src/compatibility.js
+++ src/compatibility.js
@@ -9,4 +9,39 @@ if (!Object.assign) {
     };
 }
 
+function FakeAudio () {
+}
+
+FakeAudio.prototype.play = function () {
+};
+
+if ("undefined" === typeof Audio) {
+    window.Audio = FakeAudio;
+}
+
+let video = document.createElement("video");
+if ("function" !== typeof video.play) {
+    const
+        initialCreateElement = document.createElement,
+        fakeVideo = {
+            setAttribute: () => {},
+            addEventListener: () => {},
+            // Copied from NoSleepJS
+            play: () => {
+                fakeVideo.pause();
+                fakeVideo.noSleepTimer = window.setInterval(function () {
+                    window.location.href = "/";
+                    window.setTimeout(window.stop, 0);
+                }, 15000);
+            },
+            pause: () => {
+                if (fakeVideo.noSleepTimer) {
+                    window.clearInterval(fakeVideo.noSleepTimer);
+                    fakeVideo.noSleepTimer = null;
+                }
+            }
+        };
+    document.createElement = tagName => tagName === "video" ? fakeVideo : initialCreateElement.call(document, tagName);
+}
+
 module.exports = true;
