e6cc128de5541f72e0e83efd1c071056aaa7ed4e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2017-08-29 23:43:58 -0400

    Sounds handling

--- src/sounds.js
+++ src/sounds.js
@@ -1,9 +1,35 @@
 "use strict";
 
-const noop = () => {};
+
+let
+    _mustCreate = true;
+
+const
+    noop = () => {},
+
+    _sprites = {
+        blank: {
+            from: 0,
+            to: 5
+        },
+        tick: {
+            from: 5,
+            to: 6
+        },
+        end: {
+            from: 6,
+            to: 8.5
+        }
+    },
+
+    _play = () => {
+        if (_mustCreate) {
+            _createAudio();
+        }
+    };
 
 module.exports = {
-    play: noop,
+    play: noop, // "function" === typeof Audio ? _play : noop,
     pause: noop,
     tick: noop,
     end: noop
