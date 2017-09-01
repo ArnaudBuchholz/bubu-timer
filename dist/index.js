3120e6acbcec9b2481d6ae53c01b2deab8548ad4
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-09-01 13:21:19 -0400

    SPA Basic mechanism

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,39 @@
 "use strict";
 
+let
+    _mode;
+
 const
     log = text => {
         console.log(text);
         const line = document.createElement("div");
         line.appendChild(document.createTextNode(text));
         return document.body.appendChild(line);
+    },
+
+    hashChange = () => {
+        let newMode;
+        if (!location.hash) {
+            log("No hash, setting to edit");
+            location.hash = "edit";
+        }
+        if (location.hash.indexOf("#edit") === 0) {
+            newMode = "EDIT";
+        } else if (location.hash.indexOf("#run") === 0) {
+            newMode = "RUN";
+        }
+        if (newMode !== _mode) {
+            _mode = newMode;
+            log(_mode);
+        }
     };
 
-window.addEventListener("load", function () {
-    log("loaded");
-    if (!location.hash) {
-        log("No hash, setting to edit");
-        location.hash = "edit";
-    }
-    if (location.hash === "#edit") {
-        log("EDIT");
-    } else if (location.hash === "#run") {
-        log("RUN");
-    }
+[
+    "load",
+    "hashchange"
+].forEach(eventName => {
+    window.addEventListener(eventName, function () {
+        log(`Event '${eventName}'`);
+        hashChange();
+    });
 });
