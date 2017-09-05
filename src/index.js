552e0c2f9efcbc81f0a759c8f1c83a6577eb299b
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-09-05 12:45:38 -0400

    One page to run them all

--- src/index.js
+++ src/index.js
@@ -6,22 +6,27 @@ let
     _mode;
 
 const
+    noop = require("./noop"),
     browser = require("./browser"),
     hash = require("./hash"),
     dom = require("./dom"),
     EDIT_HASH = "edit",
-    RUN_HASH = "hash",
+    RUN_HASH = "run",
     _modes = {},
 
     hashChange = () => {
         let newMode = hash.getMode(EDIT_HASH);
         if (newMode !== _mode) {
+            (_modes[_mode].teardown || noop)();
             _mode = newMode;
             dom.clear("body");
-            browser(_modes[_mode]);
+            browser.setMapping(_modes[_mode].setup());
         }
     };
 
+_modes["undefined"] = {
+    teardown: noop
+};
 _modes[EDIT_HASH] = require("./edit");
 _modes[RUN_HASH] = require("./run");
 
