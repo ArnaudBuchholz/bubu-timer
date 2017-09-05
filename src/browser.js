f540334848000965cef21636823afa0a17c2514f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-09-05 12:45:07 -0400

    One page to run them all

--- src/browser.js
+++ src/browser.js
@@ -1,52 +1,58 @@
 "use strict";
 
-const noop = () => {};
+let
+    _mapping = {};
+
+const
+    noop = require("./noop"),
+
+    coords = e => e.touches
+        ? {x: e.touches[0].clientX, y: e.touches[0].clientY}
+        : {x: e.clientX, y: e.clientY},
+
+    click = e => {
+        const {x, y} = coords(e);
+        if (Object.keys(_mapping).filter(id => id !== "undefined").every(id => {
+            const rect = document.getElementById(id).getBoundingClientRect();
+            if (!(x < rect.left || x > rect.right || y < rect.top || y > rect.bottom)) {
+                _mapping[id]();
+                return false;
+            }
+            return true;
+
+        })) {
+            (_mapping["undefined"] || noop)();
+        }
+    };
 
 require("./res/stylesheet.css");
 
-module.exports = setup => {
+window.addEventListener("load", () => {
+    let
+        touchDisabled = true,
+        touchEvent;
 
-    window.addEventListener("load", () => {
-        let
-            touchDisabled = true,
-            touchEvent;
-        const
-            mapping = setup(),
-            coords = e => e.touches
-                ? {x: e.touches[0].clientX, y: e.touches[0].clientY}
-                : {x: e.clientX, y: e.clientY},
-            click = e => {
-                const {x, y} = coords(e);
-                if (Object.keys(mapping).every(id => {
-                    if ("undefined" === id) {
-                        return true; // skip
-                    }
-                    const rect = document.getElementById(id).getBoundingClientRect();
-                    if (!(x < rect.left || x > rect.right || y < rect.top || y > rect.bottom)) {
-                        mapping[id]();
-                        return false;
-                    }
-                    return true;
-
-                })) {
-                    (mapping["undefined"] || noop)();
-                }
-            };
-
-        window.addEventListener("click", e => touchDisabled ? click(e) : 0, true);
-        window.addEventListener("touchstart", e => {
-            touchDisabled = false;
-            touchEvent = e;
-        }, false);
-        window.addEventListener("touchmove", () => {
+    window.addEventListener("click", e => touchDisabled ? click(e) : 0, true);
+    window.addEventListener("touchstart", e => {
+        touchDisabled = false;
+        touchEvent = e;
+    }, false);
+    window.addEventListener("touchmove", () => {
+        touchEvent = null;
+    }, false);
+    window.addEventListener("touchend", () => {
+        if (null !== touchEvent) {
+            click(touchEvent);
             touchEvent = null;
-        }, false);
-        window.addEventListener("touchend", () => {
-            if (null !== touchEvent) {
-                click(touchEvent);
-                touchEvent = null;
-            }
-        }, false);
-    });
+        }
+    }, false);
+
+});
+
+module.exports = {
+
+    setMapping: mapping => {
+        _mapping = mapping;
+    }
 
 };
