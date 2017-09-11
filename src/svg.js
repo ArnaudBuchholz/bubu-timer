3d0be73e314bd4721c2ad7a4f5a035fd1cb0cad5
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-09-11 13:18:31 -0400

    Handles xlink namespace

--- src/svg.js
+++ src/svg.js
@@ -1,10 +1,17 @@
 "use strict";
 
 const
+    SVG_NAMESPACE = "http://www.w3.org/2000/svg",
+    XLINK_NAMESPACE = "http://www.w3.org/1999/xlink",
+
     _tag = function (tagName, properties = {}, children = []) {
-        let element = document.createElementNS("http://www.w3.org/2000/svg", tagName);
+        let element = document.createElementNS(SVG_NAMESPACE, tagName);
         Object.keys(properties).forEach(name => {
-            element.setAttribute(name, properties[name]);
+            if (name.indexOf("xlink:") === 0) {
+                element.setAttributeNS(XLINK_NAMESPACE, name.substr(6), properties[name]);
+            } else {
+                element.setAttribute(name, properties[name]);
+            }
         });
         [].concat(children)
             .map(def => typeof def === "string" ? document.createTextNode(def) : def)
@@ -22,7 +29,8 @@ const
     "linearGradient",
     "stop",
     "rect",
-    "g"
+    "g",
+    "image"
 
 ].forEach(tag => {
     _svg[tag] = _tag.bind(null, tag);
